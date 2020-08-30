b1b532d5dc1201c806bcdcc676bc3ab3b5b0191c
Lucas Silva <luckkass89@gmail.com>
2020-08-30 20:25:39 -0300

    Inicializando projeto

---
+++ AWMonitor/AWMonitor/AWMonitor/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AWMonitor.Models;
+
+namespace AWMonitor.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
