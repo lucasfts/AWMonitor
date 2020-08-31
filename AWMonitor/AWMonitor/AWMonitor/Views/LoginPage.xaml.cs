6ebd4d51be97064a17da3d5c217b4323cde7e282
Lucas Silva <luckkass89@gmail.com>
2020-08-31 01:50:45 -0300

    Protótipos em xaml

---
+++ AWMonitor/AWMonitor/AWMonitor/Views/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AWMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new DashboardPage(); 
+        }
+
+        private void Register_Tapped(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new RegisterPage());
+        }
+
+        private void ForgetPassword_Tapped(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new ForgetPasswordPage());
+        }
+    }
+}
