cd65b3baf6ff5985f4ae3d80bb29a7bf130c1374
Lucas Silva <luckkass89@gmail.com>
2020-10-25 22:10:04 -0300

    Implementando notificações no Android

--- AWMonitor/AWMonitor/AWMonitor.Android/MainActivity.cs
+++ AWMonitor/AWMonitor/AWMonitor.Android/MainActivity.cs
@@ -6,12 +6,23 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+using AWMonitor.Services;
+using Xamarin.Forms;
 
 namespace AWMonitor.Droid
 {
     [Activity(Label = "AWMonitor", Icon = "@drawable/estatisticas", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private ISettingsService _settingsService => DependencyService.Get<ISettingsService>();
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -24,12 +35,44 @@ namespace AWMonitor.Droid
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
             ZXing.Mobile.MobileBarcodeScanner.Initialize(Application);
             LoadApplication(new App());
+
+            CreateNotificationChannel();
+            SubscribeToNotifications();
         }
+
+        private void SubscribeToNotifications()
+        {
+            FirebaseMessaging.Instance.SubscribeToTopic("actuators");
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+
     }
 }
